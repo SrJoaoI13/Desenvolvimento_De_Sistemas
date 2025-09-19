5705edf922f6ebf428b75a8ccbab11851d8cadc1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-19 20:43:55 -0300

    Salvo

---
+++ Aula_API REST/Aula4/Aula4/src/main/java/com/example/Aula4/Aula4Application.java
@@ -0,0 +1,14 @@
+package com.example.Aula4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula4Application.class, args);
+	}
+	
+
+}
