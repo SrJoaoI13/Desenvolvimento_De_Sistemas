c2b870b4dfd9b3de6e1f8958552d866de54e3606
Your Name <you@example.com>
2025-10-02 20:46:36 -0300

    Nova atividade

--- Aula_Java/Aula_API REST/Aula_8_Exceção/src/main/java/com/example/aula_8/service/UsuarioService.java
+++ Aula_Java/Aula_API REST/Aula_8_Exceção/src/main/java/com/example/aula_8/service/UsuarioService.java
@@ -14,10 +14,16 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
     
+    // Listar todos os usuarios
+    
     public List<UsuarioModel> listarTodos() {
         return usuarioRepository.findAll();
     }
 
+    // Salvar um novo usuario
+    // Verificar se o email já está cadastrado
+    //  Se estiver, lançar uma exceção
+    //  Se não estiver, salvar o usuario
     public UsuarioModel salvar(UsuarioModel usuario) {
         if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
             throw new IllegalArgumentException("Usuario já cadastrado");
@@ -26,4 +32,27 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
+//   Atualizar um usuario existente
+//   Verificar se o usuario existe
+//   Se não existir, lançar uma exceção
+//   Se existir, atualizar o usuario
+    public UsuarioModel atualizar (Long id, UsuarioModel usuario) {
+        if(!usuarioRepository.existsById(id)){
+            throw new IllegalArgumentException("Usuario não encontrado");
+        }
+            return usuarioRepository.save(usuario);
+    }
+
+    // Deletar um usuario existente
+    // Verificar se o usuario existe
+    // Se não existir, lançar uma exceção
+    // Se existir, deletar o usuario
+    public void deletar(Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new IllegalArgumentException("Usuario não encontrado");
+        }
+        usuarioRepository.deleteById(id);
+    }
+
 }
+
